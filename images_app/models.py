f9716d249db7d61d202162c6c04b25fa17fac906
maciej <mkrywult88@gmail.com>
2022-10-25 20:45:54 +0200

    first models definition

--- images_app/models.py
+++ images_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Libary(models.Model):
+    album_id = models.IntegerField(max_length=200)
+    title = models.CharField(max_length=300)
+    link = models.ImageField(upload_to='images/')
