6de321c7605d4b009b3203ec308f38c71d3a1240
maciej <mkrywult88@gmail.com>
2022-10-26 17:07:27 +0200

    update, destroy and create view

--- rest_api/vievsets.py
+++ rest_api/vievsets.py
@@ -2,7 +2,9 @@ from rest_framework import generics
 from rest_api.serializers import BookSerializer
 from images_app.models import Libary
 
-class LibaryView(generics.ListAPIView):
+class LibaryView(generics.ListCreateAPIView,
+                 generics.DestroyAPIView,
+                 generics.UpdateAPIView):
     queryset = Libary.objects.all()
     serializer_class = BookSerializer
     ordering_fields = ['id']
