5c02eaf8ba292fe2a9e8c4dee72a9b2eba127116
maciej <mkrywult88@gmail.com>
2022-10-26 17:47:16 +0200

    models update to handle synthetic id

--- images_app/models.py
+++ images_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Libary(models.Model):
-    album_id = models.IntegerField()
-    title = models.CharField(max_length=300)
-    link = models.ImageField(upload_to='images/')
+    synthetic_id = models.IntegerField(null=True, blank=True)
+    album_id = models.IntegerField(null=True, blank=True)
+    title = models.CharField(null=True, blank=True, max_length=300)
+    link = models.ImageField(null=True, blank=True, upload_to='images/')
