3af7603eaa6836fef1abe70c7ab6d58c2097fd03
maciej <mkrywult88@gmail.com>
2022-10-28 20:32:24 +0200

    small view update

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -2,10 +2,10 @@ from images_app.models import Libary
 from rest_framework import serializers
 
 
-class LibaryListSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Libary
-        fields = ('id', 'album_id', 'title', 'link', 'width', 'height')
+# class LibaryListSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Libary
+#         fields = ('id', 'album_id', 'title', 'link', 'width', 'height')
 
 class LibaryUpdateSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- rest_api/vievsets.py
+++ rest_api/vievsets.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
-from rest_api.serializers import LibaryListSerializer, LibaryUpdateSerializer
+from rest_api.serializers import LibaryUpdateSerializer
 from images_app.models import Libary
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 
 class LibaryListView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
